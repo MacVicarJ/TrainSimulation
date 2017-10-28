ff4cc1b9aa24db0f65d5ae6afb5fc3b658b8431c
Roman Yacik <32108892+yacikr@users.noreply.github.com>
2017-10-27 22:06:36 -0400

    some work done for logger class

--- src/edu/wit/dcsn/comp2000/queueapp/Logger.java
+++ src/edu/wit/dcsn/comp2000/queueapp/Logger.java
@@ -1,7 +1,29 @@
 package edu.wit.dcsn.comp2000.queueapp;
+//my class
+//-Roman
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileIOException;
 
-
-//Test comment
-
-public class Logger {
+public class Logger{
+	public void toStringPassenger(Passenger passenger) throws IOException{
+		java.io.File file = new java.io.File("TrainSimulation.log");
+		if(file.exists()) {
+			System.out.println("File already exists");
+			System.exit(1);
+		}
+		java.io.PrintWriter output = new java.io.PrintWriter(file);
+		output.print("Passenger ID: ", passenger.ID);
+	}
+	public void toStringTrain(Train train) throws IOException{
+		java.io.File file = new java.io.File("TrainSimulation.log");
+		if(file.exists()) {
+			System.out.println("File already exists");
+			System.exit(1);
+		}
+		java.io.PrintWriter output = new java.io.PrintWriter(file);
+		output.print("Train ID: ", train.ID);
+	}
+	
 }
+
--- src/edu/wit/dcsn/comp2000/queueapp/Main.java
+++ src/edu/wit/dcsn/comp2000/queueapp/Main.java
@@ -3,6 +3,6 @@ package edu.wit.dcsn.comp2000.queueapp;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	
     }
 }
