e4ae99da4c8c704a3192dcc5a8bb492a17fdb4ba
Roman Yacik <32108892+yacikr@users.noreply.github.com>
2017-10-27 23:33:26 -0400

    methods created for logging departure, arrival, and new passenger events

--- src/edu/wit/dcsn/comp2000/queueapp/Logger.java
+++ src/edu/wit/dcsn/comp2000/queueapp/Logger.java
@@ -6,16 +6,42 @@ import java.io.FileWriter;
 import java.io.FileIOException;
 
 public class Logger{
-	public void toStringPassenger(Passenger passenger) throws IOException{
+	public void toStringNewPassenger(Passenger passenger, Station station) throws IOException{
 		java.io.File file = new java.io.File("TrainSimulation.log");
 		if(file.exists()) {
 			System.out.println("File already exists");
 			System.exit(1);
 		}
+		
 		java.io.PrintWriter output = new java.io.PrintWriter(file);
 		output.print("Passenger ", passenger.ID);
+		output.println(" enters platform at station",station.ID)
 	}
-	public void toStringTrain(Train train) throws IOException{
+	public void toStringBoardingPassenger(Passenger passenger, Station station, Train train) throws IOException{
+		java.io.File file = new java.io.File("TrainSimulation.log");
+		if(file.exists()) {
+			System.out.println("File already exists");
+			System.exit(1);
+		}
+		
+		java.io.PrintWriter output = new java.io.PrintWriter(file);
+		output.print("Passenger ", passenger.ID);
+		output.print(" boards train ", train.ID);
+		output.println(" at station ", station.ID);
+	}
+	public void toStringPassengerArrival(Passenger passenger, Station station, Train train) throws IOException{
+		java.io.File file = new java.io.File("TrainSimulation.log");
+		if(file.exists()) {
+			System.out.println("File already exists");
+			System.exit(1);
+		}
+		
+		java.io.PrintWriter output = new java.io.PrintWriter(file);
+		output.print("Passenger ", passenger.ID);
+		output.print(" leaves train ", train.ID);
+		output.println(" at station ", station.ID);
+	}
+	public void toStringTrainArrival(Train train, Station station) throws IOException{
 		java.io.File file = new java.io.File("TrainSimulation.log");
 		if(file.exists()) {
 			System.out.println("File already exists");
@@ -23,6 +49,17 @@ public class Logger{
 		}
 		java.io.PrintWriter output = new java.io.PrintWriter(file);
 		output.print("Train ", train.ID);
+		output.println(" Arrives at Station ",station.ID);
+	}
+	public void toStringTrainDeparture(Train train, Station station) throws IOException{
+		java.io.File file = new java.io.File("TrainSimulation.log");
+		if(file.exists()) {
+			System.out.println("File already exists");
+			System.exit(1);
+		}
+		java.io.PrintWriter output = new java.io.PrintWriter(file);
+		output.print("Train ", train.ID);
+		output.println(" Departs from Station ",station.ID);
 	}
 	
 }
